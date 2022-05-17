e4793e5380bd1933e3829e4f4eddf1efe69ee628
mgregorio-godaddy <mgregorio@godaddy.com>
2022-05-17 10:15:42 -0300

    Embbeded Maven support added

--- src/main/java/eu/janinko/Andaria/ultimasdk/StaticDrawer.java
+++ src/main/java/eu/janinko/Andaria/ultimasdk/StaticDrawer.java
@@ -1,25 +1,5 @@
 package eu.janinko.Andaria.ultimasdk;
 
-import eu.janinko.Andaria.ultimasdk.files.Arts;
-import eu.janinko.Andaria.ultimasdk.files.Map;
-import eu.janinko.Andaria.ultimasdk.files.Statics;
-import eu.janinko.Andaria.ultimasdk.files.TileData;
-import eu.janinko.Andaria.ultimasdk.files.arts.Art;
-import eu.janinko.Andaria.ultimasdk.files.map.MapTile;
-import eu.janinko.Andaria.ultimasdk.files.statics.Static;
-import eu.janinko.Andaria.ultimasdk.files.tiledata.ItemData;
-import eu.janinko.Andaria.ultimasdk.files.tiledata.TileFlag;
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import javax.imageio.ImageIO;
-
 /**
  * @author Honza Brázdil <jbrazdil@redhat.com>
  */
